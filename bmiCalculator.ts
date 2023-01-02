7c89c6fa30ad88557b344bfee9a119188555f5e6
MDiMicelli <mdimicelli@northwell.edu>
2023-01-01 20:56:13 -0500

    finish 9.5

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,31 +1,35 @@
-function calculateBmi(heightCm: number, weightKg: number) {
+export function calculateBmi(heightCm: number, weightKg: number) {
     const heightM: number = heightCm * 0.01;
     const bmi: number = weightKg / (heightM ** 2);
-    if (bmi >= 18.5 && bmi <= 24.9) {
+    if (bmi < 18.5) {
+        return 'Underweight';
+    }
+    else if (bmi >= 18.5 && bmi <= 24.9) {
         return 'Normal (healthy weight)';
-    } else if (bmi > 24.9) {
+    } else {
         return 'Overweight';
     }
 }
-const input: Array<string> = process.argv;
-if (input.length < 3) {
-    throw new Error('Must provide a height and a weight');
-} else if (input.length > 4) {
-    throw new Error('Too many arguments given');
-}
-let heightCm: number;
-let weightKg: number;
-if(isNaN(Number(process.argv[2]))) {
-    throw new Error('the first argument, the height in cm, must be a number');
-} else if (isNaN(Number(process.argv[3]))) {
-    throw new Error('the 2nd argument, the weight in kg, must be a number');
-} else {
-    heightCm = Number(process.argv[2]);
-    weightKg = Number(process.argv[3]);
-}
-
-try {
-    console.log(calculateBmi(heightCm, weightKg));
-} catch (e) {
-    console.log('Problem running script:', e.message);
-}
+// const input: Array<string> = process.argv;
+// if (input.length < 3) {
+//     throw new Error('Must provide a height and a weight');
+// } else if (input.length > 4) {
+//     throw new Error('Too many arguments given');
+// }
+// let heightCm: number;
+// let weightKg: number;
+// if(isNaN(Number(process.argv[2]))) {
+//     throw new Error('the first argument, the height in cm, must be a number');
+// } else if (isNaN(Number(process.argv[3]))) {
+//     throw new Error('the 2nd argument, the weight in kg, must be a number');
+// } else {
+//     heightCm = Number(process.argv[2]);
+//     weightKg = Number(process.argv[3]);
+// }
+//
+// try {
+//     console.log(calculateBmi(heightCm, weightKg));
+// } catch (e) {
+//     console.log('Problem running script:', e.message);
+// }
+//
--- index.ts
+++ index.ts
@@ -1,9 +1,26 @@
 import express from 'express';
+import { calculateBmi } from "./bmiCalculator";
+
 const app = express();
 
+app.get('/bmi' , (req, res) => {
+        const { weight, height } = req.query;
+        console.log(weight, height);
+        if (typeof weight !== 'undefined' && typeof height !== 'undefined' && !isNaN(Number(weight)) && !isNaN(Number(height))) {
+            try {
+                const msg = calculateBmi(+height, +weight);
+                res.json(msg);
+            } catch (e) {
+                res.status(400).json(e);
+            }
+        } else {
+            res.status(400).json('The height and weight query parameters must be positive integers');
+        }
+});
 app.get('/saludo', (_req, res) => {
     res.send('Hello Full Stack!');
 });
+
 
 const PORT = 3003;
 
